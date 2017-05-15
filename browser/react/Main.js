18233a0e61ff44213040060d344d35b5f0cec8f9
Aria Moraine <ariamoraine@gmail.com>
2017-05-15 12:06:30 -0400

    Added in first react component

---
+++ browser/react/Main.js
@@ -0,0 +1,12 @@
+import React, { Component }from 'react';
+
+export default class Main extends Component {
+  render () {
+    return (
+      <div id='main' className='container-fluid'>
+        <h1>Hello World!!!!</h1>
+      </div>
+    );
+  }
+
+}
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,3 +1,11 @@
 'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main';
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+);
 
 console.log('Hello React');
